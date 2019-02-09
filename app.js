dd01abe1e43466d06c1ef78db7cda91c2ac82106
yassine birig <gobirig@gmail.com>
2019-02-09 16:00:56 +0100

    appjs footer

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body.list)
     let item = req.body.newItem;
     if (req.body.list === "Work") {
         workList.push(item);
